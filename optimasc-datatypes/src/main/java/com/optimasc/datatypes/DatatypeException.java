b4f5028032d65b830ac8198ad60b8c620ee0daad
Carl Eric Codere <42296635+ccodere@users.noreply.github.com>
2023-05-17 09:05:26 +0800

    + Add new error codes aligned with SQL2003 data exceptions when possible

--- optimasc-datatypes/src/main/java/com/optimasc/datatypes/DatatypeException.java
+++ optimasc-datatypes/src/main/java/com/optimasc/datatypes/DatatypeException.java
@@ -6,17 +6,41 @@ package com.optimasc.datatypes;
 
 
 
-/** 
+/** Exception codes. The error code values should be equal in values
+ *  than those defined in SQL2003 standard when possible.
  *
- * @author ccodere
+ * @author Carl Eric Codere.
  */
 public class DatatypeException extends Exception
 {
   private int code; 
-  /** Exception is thrown when the value validated is not legal
-   *  according to its datatype.
+  /** Code is set when value does not match data type
+   *  specification. This is the catch all generic error.
    */
-  public static final int ILLEGAL_VALUE  = 0;
+  public static final int ERROR_ILLEGAL_VALUE  = 0x22000;
+  
+  /** Code is set when character is not allowed in this
+   *  cahracter set.
+   */
+  public static final int ERROR_ILLEGAL_CHARACTER  = 0x22021;
+  
+  /** Code is set when the numeric value is out of range.
+   */
+  public static final int ERROR_NUMERIC_OUT_OF_RANGE  = 0x22003;
+  /** Code is set when the string is not of correct length.
+   */
+  public static final int ERROR_STRING_ILLEGAL_LENGTH  = 0x22026;
+  /** Code is set when the string will be truncated because
+   *  its length is bigger than allowed length.
+   */
+  public static final int ERROR_STRING_TRUNCATION  = 0x22001;
+  
+  /** Code is set when a datetime is not within range.
+   */
+  public static final int ERROR_ILLEGAL_DATETIME  = 0x22007;
+  
+  
+  
   /** Exception is thrown when adding a value or a symbol that
    *  is already present and duplicates are not allowed.
    */
@@ -33,6 +57,15 @@ public class DatatypeException extends Exception
     this.code = code;
   }
   
+  
+  
+  public int getCode()
+  {
+    return code;
+  }
+
+
+
   public static void throwIt(int code, String s) throws DatatypeException 
   {
      throw new DatatypeException(code,s);
